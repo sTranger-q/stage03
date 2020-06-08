148b715f9082b309270af27155a71275b9ccefd6
sTranger <853561128@qq.com>
2020-06-08 17:57:06 +0800

    day10

--- day10/js/index.js
+++ day10/js/index.js
@@ -11,12 +11,22 @@
 $().ready(function () {
     $('nav a').on('click', function () {
         // 找祖先元素
-        var parents=$(this).parents('.grade');
+        var parents = $(this).parents('.grade');
         if (parents.length) {
-                $('.grade a').removeClass('text-danger');
-        }else{
+            $('.grade a').removeClass('text-danger');
+        } else {
             $('.campus a').removeClass('text-danger');
         }
         $(this).addClass('text-danger');
     })
+})
+$(document).on('scroll', function () {
+    var scrollH = $(document).height();
+    var scrollT = Math.ceil($(document).scrollTop());
+    var clientH = $(window).height();
+    var newE= $( '<div class="row"></div>')
+    if (scrollH-scrollT - clientH<=150) {
+        
+    }
+
 })
