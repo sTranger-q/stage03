98404a5cce74e5e7781047ab2a8fb37cb88d217d
sTranger <853561128@qq.com>
2020-06-09 08:55:44 +0800

    all

--- day10/js/index.js
+++ day10/js/index.js
@@ -24,9 +24,98 @@ $(document).on('scroll', function () {
     var scrollH = $(document).height();
     var scrollT = Math.ceil($(document).scrollTop());
     var clientH = $(window).height();
-    var newE= $( '<div class="row"></div>')
-    if (scrollH-scrollT - clientH<=150) {
+    var newE= $( `
+    <div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-1.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
         
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-1.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-1.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-1.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-2.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+        
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-2.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-2.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-2.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-3.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+        
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-3.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-3.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-6">
+        <a href="#" class="thumbnail">
+            <img src="./img/news-img-3.jpg" class='img-responsive' alt="">    
+            <p class="text-muted">优秀作品</p>
+        </a>
+    </div>
+    </div>
+    `)
+
+    if (scrollH-scrollT - clientH<=150) {
+        $('.add').append(newE);
     }
 
 })
